bcae4bca119e32491751f0f5b3cff72e1a5b39a8
Marcos Bertolotti <marcos.bertolotti@hotmail.com>
2022-06-18 21:14:30 -0300

    change favicon

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,30 +8,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatCardModule } from '@angular/material/card';
-import { MatSelectModule } from '@angular/material/select';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatTableModule } from '@angular/material/table';
-
 // PIPES
 import { FieldRequiredPipe } from './pipes/field-required.pipe';
+import { ErrorMessagePipe } from './pipes/error-message.pipe';
 
 // COMPONENTS
 import { RouterComponent } from './components/router/router.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 import { TitleComponent } from './components/title/title.component';
-import { ErrorMessagePipe } from './pipes/error-message.pipe';
 import { SlideToggleComponent } from './components/slide-toggle/slide-toggle.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MatListModule } from '@angular/material/list';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
@@ -53,18 +40,6 @@ import { MatDividerModule } from '@angular/material/divider';
     FormsModule,
     ReactiveFormsModule,
     MatSlideToggleModule,
-    MatListModule,
-
-    MatFormFieldModule,
-    MatAutocompleteModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatChipsModule,
-    MatRadioModule,
-    MatDividerModule,
-    MatTableModule,
   ],
   exports: [
     RouterComponent,
