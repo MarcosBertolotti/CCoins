27e2b9092667f42f6afc4d3a35d06270779ef32d
Marcos Bertolotti <marcos.bertolotti@hotmail.com>
2022-06-25 20:25:23 -0300

    added CI firebase support

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ import { Environment } from "src/app/models/environment.model";
 
 export const environment: Environment = {
   production: true,
-  API_URL: 'http://localhost:8000/bff-app',
+  API_URL: 'https://ccoins-bff.herokuapp.com/bff-app',
   AUTH_GOOGLE_CLIENT_ID: '361460326222-60aal4bkqjt0t0mpl42s4ruo3qm9lpqn.apps.googleusercontent.com',
   AUTH_FACEBOOK_APPLICATION_ID: '321031650211530',
 };
